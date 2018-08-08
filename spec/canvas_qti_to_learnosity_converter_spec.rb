4dc6a961367262389c2d0141adfc53f1026cecb1
dspencer001 <dspencer577@gmail.com>
2018-08-08 16:19:47 -0600

    Fix the specs, remove unneeded fixtures

--- spec/canvas_qti_to_learnosity_converter_spec.rb
+++ spec/canvas_qti_to_learnosity_converter_spec.rb
@@ -44,18 +44,15 @@ RSpec.describe CanvasQtiToLearnosityConverter do
       expect(learnosity[:type]).to eq "mcq"
       expect(learnosity[:stimulus]).to eq "<div><p>The grand canyon is deep?</p></div>"
 
+      expect(learnosity[:options]).to eq [
+        {"value"=>"7161", "label"=>"True"},
+        {"value"=>"460", "label"=>"False"},
+      ]
+
       expect(learnosity[:validation]).to eq({
-        "scoring_type" => "partialMatch",
-        "valid_response" => {
-          "value" => ["right 1", "right 2", "right 3"],
-        }
+        "scoring_type" => "exactMatch",
+        "valid_response" => { "value"=>["7161"] },
       })
-
-      expect(learnosity[:possible_responses]).to eq([
-        "right 1", "right 2", "right 3", "wrong 1", "wrong 2", "wrong 3"
-      ])
-
-      expect(learnosity[:duplicate_responses]).to eq(true)
     end
   end
 
@@ -68,20 +65,22 @@ RSpec.describe CanvasQtiToLearnosityConverter do
       learnosity = question.to_learnosity
 
       expect(learnosity[:type]).to eq "mcq"
-      expect(learnosity[:stimulus]).to eq "<div><p>matching question</p></div>"
+      expect(learnosity[:stimulus]).to eq "<div><p>Which are prime?</p></div>"
+      expect(learnosity[:options]).to eq [
+        {"value"=>"945", "label"=>""},
+        {"value"=>"6532", "label"=>""},
+        {"value"=>"9078", "label"=>"3"},
+        {"value"=>"5022", "label"=>"5"},
+        {"value"=>"907", "label"=>"6"},
+        {"value"=>"9720", "label"=>"7"},
+      ]
+
       expect(learnosity[:validation]).to eq({
-        "scoring_type" => "partialMatch",
-        "valid_response" => {
-          "value" => ["right 1", "right 2", "right 3"],
-        }
+        "scoring_type"=>"partialMatch",
+        "alt_responses"=>["9078", "5022", "9720"]
       })
 
-      expect(learnosity[:possible_responses]).to eq([
-        "right 1", "right 2", "right 3", "wrong 1", "wrong 2", "wrong 3"
-      ])
-
-      expect(learnosity[:duplicate_responses]).to eq(true)
-
+      expect(learnosity[:multiple_responses]).to eq true
     end
   end
 
@@ -147,20 +146,17 @@ RSpec.describe CanvasQtiToLearnosityConverter do
 
       learnosity = question.to_learnosity
 
-      expect(learnosity[:type]).to eq "clozedropdown"
-      expect(learnosity[:stimulus]).to eq ""
-      expect(learnosity[:template]).to eq "<div><p>multiple dropdowns {{response}} {{response}}</p></div>"
+      expect(learnosity[:type]).to eq "shorttext"
+      expect(learnosity[:stimulus]).to eq "<div><p>Fill in the</p></div>"
       expect(learnosity[:validation]).to eq({
-        "scoring_type" => "partialMatch",
-        "valid_response" => {
-          "value" => ["right", "right"],
-        }
+        "scoring_type"=>"exactMatch",
+        "valid_response"=>{"value"=>"Blank"},
+        "alt_responses"=>[
+          {"value"=>"blank"},
+          {"value"=>"space"},
+          {"value"=>"empty spot"},
+        ]
       })
-
-      expect(learnosity[:possible_responses]).to eq([
-        ["right", "wrong"],
-        ["right", "wrong"],
-      ])
     end
   end
 
@@ -172,20 +168,8 @@ RSpec.describe CanvasQtiToLearnosityConverter do
 
       learnosity = question.to_learnosity
 
-      expect(learnosity[:type]).to eq "clozedropdown"
-      expect(learnosity[:stimulus]).to eq ""
-      expect(learnosity[:template]).to eq "<div><p>multiple dropdowns {{response}} {{response}}</p></div>"
-      expect(learnosity[:validation]).to eq({
-        "scoring_type" => "partialMatch",
-        "valid_response" => {
-          "value" => ["right", "right"],
-        }
-      })
-
-      expect(learnosity[:possible_responses]).to eq([
-        ["right", "wrong"],
-        ["right", "wrong"],
-      ])
+      expect(learnosity[:type]).to eq "longtextV2"
+      expect(learnosity[:stimulus]).to eq "<div><p>What do you think?</p></div>"
     end
   end
 
@@ -197,20 +181,8 @@ RSpec.describe CanvasQtiToLearnosityConverter do
 
       learnosity = question.to_learnosity
 
-      expect(learnosity[:type]).to eq "clozedropdown"
-      expect(learnosity[:stimulus]).to eq ""
-      expect(learnosity[:template]).to eq "<div><p>multiple dropdowns {{response}} {{response}}</p></div>"
-      expect(learnosity[:validation]).to eq({
-        "scoring_type" => "partialMatch",
-        "valid_response" => {
-          "value" => ["right", "right"],
-        }
-      })
-
-      expect(learnosity[:possible_responses]).to eq([
-        ["right", "wrong"],
-        ["right", "wrong"],
-      ])
+      expect(learnosity[:type]).to eq "fileupload"
+      expect(learnosity[:stimulus]).to eq "<div><p>Give me a good file.</p></div>"
     end
   end
 
@@ -222,20 +194,25 @@ RSpec.describe CanvasQtiToLearnosityConverter do
 
       learnosity = question.to_learnosity
 
-      expect(learnosity[:type]).to eq "clozedropdown"
+      expect(learnosity[:type]).to eq "clozetext"
       expect(learnosity[:stimulus]).to eq ""
-      expect(learnosity[:template]).to eq "<div><p>multiple dropdowns {{response}} {{response}}</p></div>"
+      expect(learnosity[:template]).to eq "<div><p>Roses are {{response}}, violets are {{response}}</p></div>"
       expect(learnosity[:validation]).to eq({
         "scoring_type" => "partialMatch",
         "valid_response" => {
-          "value" => ["right", "right"],
-        }
+          "value"=>["Red", "Blue"]
+        },
+        "alt_responses" => [
+          {"value"=>["Red", "BLUE"]},
+          {"value"=>["Red", "blue"]},
+          {"value"=>["red", "Blue"]},
+          {"value"=>["red", "BLUE"]},
+          {"value"=>["red", "blue"]},
+          {"value"=>["RED", "Blue"]},
+          {"value"=>["RED", "BLUE"]},
+          {"value"=>["RED", "blue"]},
+        ]
       })
-
-      expect(learnosity[:possible_responses]).to eq([
-        ["right", "wrong"],
-        ["right", "wrong"],
-      ])
     end
   end
 
